281e557ebcd9e9f54679103c26d8ae6221a267f5
zhongning <xyzning@qq.com>
2019-08-28 17:30:54 +0800

    QRcode auto get QRcode

---
+++ QRcode/QRcode.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace QRcode
+{
+    partial class QRcode
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QRcode));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 292);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 350);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SSID :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 350);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // QRcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 414);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "QRcode";
+            this.Text = "QRcode";
+            this.Load += new System.EventHandler(this.QRcode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QRcode/QRcode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using System.Drawing.Imaging;
+using System.IO;
+using cmd;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace QRcode
+{
+    public partial class QRcode : Form
+    {
+        private System.Timers.Timer myTimer;
+        public QRcode()
+        {
+            InitializeComponent();
+        }
+
+        private string execCommand(string exeName, string argName)
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = exeName;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = argName;
+            p.Start();
+            p.WaitForExit();
+            return p.StandardOutput.ReadToEnd();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void QRcode_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;//不是很合理的做法
+            textBox1.Text = "";
+            pictureBox1.Image = null;
+            this.myTimer = new System.Timers.Timer(1000);
+            this.myTimer.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Tick);
+            this.myTimer.AutoReset = true;
+            this.myTimer.Enabled = true;
+            this.myTimer.Start();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string check_devices = execCommand("adb", "devices");
+            string[] str = check_devices.Split('\n');
+            if (str[1].Length > 6)
+            {
+                string ssid = execCommand("adb", "shell cat /dev/atel_misc");
+                if (ssid.Length > 3)
+                {
+                    textBox1.Text = ssid;
+                    QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+                    QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(ssid, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qRCode = new QRCode(qRCodeData);
+                    Bitmap bitmap = qRCode.GetGraphic(9);
+                    pictureBox1.Image = bitmap;
+                }
+            }
+            else
+            {
+                textBox1.Text = "";
+                pictureBox1.Image = null;
+            }
+        }
+    } 
+}
